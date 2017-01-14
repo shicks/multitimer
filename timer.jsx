7b14fa68228de77d7c631354f4df1fd6f184cbc2
Stephen Hicks <sdh33@cornell.edu>
2017-01-13 22:19:12 -0800

    initial commit

---
+++ timer.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class Timer extends React.Component {
+  render() {
+    return (
+        <div className="timer">
+          <div className="time" />
+          <div className="name" />
+        </div>
+    );
+  }
+}
+      
