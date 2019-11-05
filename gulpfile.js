8485354ed2fa28985528bfe65b888f481f554e62
Stephen Hicks <stephenhicks@gmail.com>
2019-11-05 13:09:38 -0800

    try to get gulp deploy working

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp');
 const closureCompiler = require('google-closure-compiler').gulp();
+const ghPages = require('gulp-gh-pages');
 
-gulp.task('default', () =>
+gulp.task('js', () =>
     gulp.src(['timer.js', 'externs.js'])
           .pipe(closureCompiler({
             compilation_level: 'ADVANCED',
@@ -10,4 +11,14 @@ gulp.task('default', () =>
             language_out: 'ECMASCRIPT5_STRICT',
             output_wrapper: '(function(){\n%output%\n}).call(this)',
             js_output_file: 'timer-transpiled.js'}))
-          .pipe(gulp.dest('.')));
+          .pipe(gulp.dest('./dist')));
+
+gulp.task('copy', () =>
+    gulp.src(['index.html', 'timer.css'])
+          .pipe(gulp.dest('./dist')));
+
+gulp.task('default', gulp.series(['js', 'copy'], () => Promise.resolve()));
+
+gulp.task('deploy', () =>
+    gulp.src('./dist/**/*')
+          .pipe(ghPages()));
